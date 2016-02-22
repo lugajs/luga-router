af30171512f47916ac990c2bd0d3f6f4f041c915
Massimo Foti <massimo@massimocorner.com>
2016-02-22 18:41:58 +0100

    navigate: Added utility functions

--- src/luga.navigate.js
+++ src/luga.navigate.js
@@ -1,7 +1,27 @@
 (function(){
 	"use strict";
 
-	luga.navigate = function(){
+	var config = {
+		pushState: false,
+		root: "/"
+	};
+
+	luga.navigateSetup = function(options){
+		luga.merge(config, options);
+		return config;
+	};
+
+	/* istanbul ignore next */
+	luga.isPushStateSupported = function(){
+		// Only IE9 should return false
+		return (history.pushState !== undefined);
+	};
+
+	luga.usePushState = function(){
+		return ((config.pushState === true) && (luga.isPushStateSupported() === true));
+	};
+
+	luga.navigate = function(fragment, options){
 
 	};
 
--- test/spec/luga.navigate.Spec.js
+++ test/spec/luga.navigate.Spec.js
@@ -1,8 +1,71 @@
-describe("luga.navigate()", function(){
+describe("luga", function(){
 
 	"use strict";
 
 	beforeEach(function(){
+
+	});
+
+	afterEach(function(){
+		luga.navigateSetup({
+			pushState: false,
+			root: "/"
+		});
+	});
+
+	describe(".navigateSetup()", function(){
+
+		describe("If called with no arguments. Return an object containing name/value pairs:", function(){
+
+			it("pushState = false", function(){
+				expect(luga.navigateSetup().pushState).toEqual(false);
+			});
+			it("root = '/'", function(){
+				expect(luga.navigateSetup().root).toEqual("/");
+			});
+
+		});
+
+		describe("If a set of name/value pairs is passed as argument. Set the following configuration options:", function(){
+
+			it("pushState", function(){
+				expect(luga.navigateSetup({pushState: true}).pushState).toEqual(true);
+			});
+			it("root", function(){
+				expect(luga.navigateSetup({root: "/app/subappa/"}).root).toEqual("/app/subappa/");
+			});
+
+		});
+
+	});
+
+
+	describe(".usePushState()", function(){
+
+		describe("Return false:", function(){
+
+			it("By default", function(){
+				spyOn(luga, "isPushStateSupported").and.returnValue(true);
+				expect(luga.usePushState()).toEqual(false);
+			});
+
+			it("If pushState is not supported, not matter the value of config.pushState", function(){
+				spyOn(luga, "isPushStateSupported").and.returnValue(false);
+				luga.navigateSetup({pushState: true});
+				expect(luga.usePushState()).toEqual(false);
+			});
+
+		});
+
+		describe("Return true if:", function(){
+
+			it("pushState is supported and config.pushState has been set to true", function(){
+				spyOn(luga, "isPushStateSupported").and.returnValue(true);
+				luga.navigateSetup({pushState: true});
+				expect(luga.usePushState()).toEqual(true);
+			});
+
+		});
 
 	});
 
