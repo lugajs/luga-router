aee2acf126b645628f04512f51f48c3546043068
Massimo Foti <massimo@massimocorner.com>
2016-07-03 16:59:33 +0200

    luga.Router skeleton

--- test/spec/luga.router.Spec.js
+++ test/spec/luga.router.Spec.js
@@ -1,19 +1,27 @@
-describe("luga.router", function(){
+describe("luga.Router", function(){
 
 	"use strict";
 
+	var emptyRouter;
 	beforeEach(function(){
-
+		emptyRouter = new luga.Router();
 	});
 
-	it("Lives inside its own namespace", function(){
-		expect(luga.router).toBeDefined();
+	it("Is the Router constructor", function(){
+		expect(luga.Router).toBeDefined();
 	});
 
 	describe(".version", function(){
 		it("Reports the current version number", function(){
-			expect(luga.router.version).toBeDefined();
+			expect(luga.Router.version).toBeDefined();
 		});
+	});
+
+	it("Implements the luga.Notifier interface", function(){
+		var MockNotifier = function(){
+			luga.extend(luga.Notifier, this);
+		};
+		expect(emptyRouter).toMatchDuckType(new MockNotifier());
 	});
 
 });
