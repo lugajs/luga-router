8a0e79862a092604df1b3efe3b522bc15a80c351
Massimo Foti <massimo@massimocorner.com>
2016-07-22 19:50:36 +0200

    Added luga.router.isValidRoute()

--- src/luga.router.js
+++ src/luga.router.js
@@ -9,4 +9,21 @@ if(typeof(luga) === "undefined"){
 	luga.namespace("luga.router");
 	luga.router.version = "0.1.0";
 
+	/**
+	 * Return true if the given object implements the luga.router.iRoute interface. False otherwise
+	 * @param {*} obj
+	 * @returns {boolean}
+	 */
+	luga.router.isValidRoute = function(obj){
+		if(luga.type(obj) === "object"){
+			if((luga.type(obj.path) === "string") &&
+				(luga.type(obj.enter) === "function") &&
+				(luga.type(obj.exit) === "function") &&
+				(luga.type(obj.match) === "function")){
+				return true;
+			}
+		}
+		return false;
+	};
+
 }());
--- test/spec/luga.router.Spec.js
+++ test/spec/luga.router.Spec.js
@@ -2,19 +2,35 @@ describe("luga.router", function(){
 
 	"use strict";
 
-	var baseRouter;
-	beforeEach(function(){
-		baseRouter = new luga.router.Router();
-	});
-
 	it("Lives inside its own namespace", function(){
 		expect(luga.router).toBeDefined();
 	});
 
 	describe(".version", function(){
-		it("Reports the current version number", function(){
+		it("Report the current version number", function(){
 			expect(luga.router.version).toBeDefined();
 		});
+	});
+
+	describe(".isValidRoute()", function(){
+
+		it("Return true if the given object implements the luga.router.iRoute interface", function(){
+
+			var goodRute = new luga.router.Route({
+				path: "ciccio",
+				enterCallBacks: [],
+				exitCallBacks: []
+			});
+
+			expect(luga.router.isValidRoute(goodRute)).toEqual(true);
+		});
+
+		it("Return false otherwise", function(){
+			expect(luga.router.isValidRoute(new luga.router.Router())).toEqual(false);
+			expect(luga.router.isValidRoute({})).toEqual(false);
+			expect(luga.router.isValidRoute("test")).toEqual(false);
+		});
+
 	});
 
 });
